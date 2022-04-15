b76b23f5be9aa986cff9874667527141ede15c4a
Ben Lambeth <accounts@lambethben.com>
2022-04-15 13:33:09 -0400

    Rename app

--- RoundtableHold/RoundtableHold/RoundtableHold.Android/MainActivity.cs
+++ RoundtableHold/RoundtableHold/RoundtableHold.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace RoundtableHold.Droid
 {
-    [Activity(Label = "RoundtableHold", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Elden Ring Guides", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
